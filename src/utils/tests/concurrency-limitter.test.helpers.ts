17ec31f4af41d6a376360520c233d815dbe8a62a
Sela Marjan Almog <sela.marjan@gmail.com>
2020-12-07 15:15:32 +0200

    test cuncurrency limitter

---
+++ src/utils/tests/concurrency-limitter.test.helpers.ts
@@ -0,0 +1,39 @@
+export const createConcurrencyTracker = () => {
+    let counter: number = 0;
+    let max: number = 0;
+    let executionTimestamps: number[] = [];
+
+    const add = () => {
+        executionTimestamps.push(Date.now());
+        counter++;
+        max = Math.max(max, counter);
+    }
+
+    const remove = () => {
+        counter--;
+    }
+
+    const getMax = () => max;
+
+    const getExecutionTimestamps = () => executionTimestamps;
+
+    const trackedAsyncAction = () => {
+        return new Promise(resolve => {
+            add();
+            setTimeout(() => {
+                remove();
+                resolve();
+            });
+        })
+    }
+
+    return {
+        concurrencyTracker: {
+            getMax,
+            getExecutionTimestamps
+        },
+        trackedAsyncAction
+    }
+}
+
+export const repeat = (count: number, action: () => any) => new Array(count).fill({}).map(action);
---
+++ src/utils/tests/concurrency-limitter.test.ts
@@ -0,0 +1,48 @@
+import { createConcurrencyLimitter } from "../concurrency-limitter";
+import { createConcurrencyTracker, repeat } from './concurrency-limitter.test.helpers';
+
+describe('concurrency-limitter', () => {
+
+    it('should allow up to max concurrent asyncActions', async () => {
+        //given
+        const maxConcurrent = 3;
+        const executionCount = 10;
+
+        const concurrencyLimitter = createConcurrencyLimitter(maxConcurrent);
+        const { concurrencyTracker, trackedAsyncAction } = createConcurrencyTracker();
+
+        //when
+        const executionPromises = repeat(executionCount, () => concurrencyLimitter.add(trackedAsyncAction));
+
+        await Promise.all(executionPromises);
+
+        //assert
+        expect(concurrencyTracker.getMax()).toBeLessThanOrEqual(maxConcurrent);
+    });
+
+    it('should pause executions, and resume after timeout', async () => {
+        //given
+        const timeout = 200;
+        const maxConcurrent = 3;
+        const executionCount = 10;
+
+        const concurrencyLimitter = createConcurrencyLimitter(maxConcurrent);
+        const { concurrencyTracker, trackedAsyncAction } = createConcurrencyTracker();
+
+        //when
+        const executionPromises = repeat(executionCount, () => concurrencyLimitter.add(trackedAsyncAction));
+        concurrencyLimitter.pause(timeout);
+
+        await Promise.all(executionPromises);
+
+        //assert
+        const executionTimestamps = concurrencyTracker.getExecutionTimestamps();
+        const beforePauseExecution = executionTimestamps[maxConcurrent-1];
+        const afterPauseExecution = executionTimestamps[maxConcurrent];
+
+        const diff = afterPauseExecution - beforePauseExecution;
+
+        expect(diff).toBeGreaterThanOrEqual(timeout);
+    });
+
+});
